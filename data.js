fbff472c07e2edb233458cd2fa00416d883009f8
Wayne Pacileo <wsfpacileo@gmail.com>
2020-03-09 16:37:19 -0400

    added circles
    added circles with data dependant styling

--- data.js
+++ data.js
@@ -1,443 +1,149 @@
 
+//Data Source: NYC Open Data: Harbor Water Quality Data Set
+
 var testlocationData = [
   {
-    "shortname": "AC1",
-    "lng": -73.75175,
-    "lat": 40.76317
-  },
-  {
-    "shortname": "BB2",
+    "shortName": "BB2",
     "lng": -73.82216,
-    "lat": 40.66004
+    "lat": 40.66004,
+    "longName": "Head of Bergen Basin"
   },
   {
-    "shortname": "BB4",
+    "shortName": "BB4",
     "lng": -73.82316,
-    "lat": 40.65164
+    "lat": 40.65164,
+    "longName": "Mouth of Bergen Basin"
   },
   {
-    "shortname": "BR1",
-    "lng": -73.86271,
-    "lat": 40.89314
-  },
-  {
-    "shortname": "BR3",
-    "lng": -73.8848,
-    "lat": 40.82537
-  },
-  {
-    "shortname": "BR5",
-    "lng": -73.87133,
-    "lat": 40.81367
-  },
-  {
-    "shortname": "CIC3",
-    "lng": -73.99417,
-    "lat": 40.58033
-  },
-  {
-    "shortname": "E10",
-    "lng": -73.76533,
-    "lat": 40.84367
-  },
-  {
-    "shortname": "E11",
-    "lng": -73.76533,
-    "lat": 40.84367
-  },
-  {
-    "shortname": "E12",
-    "lng": -73.80945,
-    "lat": 40.85444
-  },
-  {
-    "shortname": "E13",
-    "lng": -73.84111,
-    "lat": 40.80611
-  },
-  {
-    "shortname": "E14",
-    "lng": -73.8645,
-    "lat": 40.80083
-  },
-  {
-    "shortname": "E15",
-    "lng": -73.8495,
-    "lat": 40.763
-  },
-  {
-    "shortname": "E2",
-    "lng": -73.97,
-    "lat": 40.73533
-  },
-  {
-    "shortname": "E4",
-    "lng": -73.922,
-    "lat": 40.7825
-  },
-  {
-    "shortname": "E6",
-    "lng": -73.86083,
-    "lat": 40.7855
-  },
-  {
-    "shortname": "E7",
-    "lng": -73.8205,
-    "lat": 40.805
-  },
-  {
-    "shortname": "E8",
-    "lng": -73.8205,
-    "lat": 40.805
-  },
-  {
-    "shortname": "F1",
+    "shortName": "F1",
     "lng": -73.88413,
-    "lat": 40.64315
+    "lat": 40.64315,
+    "longName": "Mouth of Fresh Creek"
   },
   {
-    "shortname": "F5",
+    "shortName": "F5",
     "lng": -73.87896,
-    "lat": 40.63878
+    "lat": 40.63878,
+    "longName": "Head of Fresh Creek"
   },
   {
-    "shortname": "FB1",
-    "lng": -73.85417,
-    "lat": 40.77267
-  },
-  {
-    "shortname": "FLC1",
-    "lng": -73.83667,
-    "lat": 40.76217
-  },
-  {
-    "shortname": "FLC2",
-    "lng": -73.84283,
-    "lat": 40.765
-  },
-  {
-    "shortname": "G2",
-    "lng": -74.00333,
-    "lat": 40.66667
-  },
-  {
-    "shortname": "GB1",
-    "lng": -74.01367,
-    "lat": 40.6
-  },
-  {
-    "shortname": "GC3",
-    "lng": -73.98824,
-    "lat": 40.67943
-  },
-  {
-    "shortname": "GC4",
-    "lng": -73.98907,
-    "lat": 40.67803
-  },
-  {
-    "shortname": "GC5",
-    "lng": -73.99005,
-    "lat": 40.67614
-  },
-  {
-    "shortname": "GC6",
-    "lng": -73.99639,
-    "lat": 40.67417
-  },
-  {
-    "shortname": "GHC",
-    "lng": -73.82083,
-    "lat": 40.64533
-  },
-  {
-    "shortname": "H3",
-    "lng": -73.93383,
-    "lat": 40.828
-  },
-  {
-    "shortname": "HC1",
+    "shortName": "HC1",
     "lng": -73.87891,
-    "lat": 40.65359
+    "lat": 40.65359,
+    "longName": "Hendrix Creek Head"
   },
   {
-    "shortname": "HC2",
+    "shortName": "HC2",
     "lng": -73.87684,
-    "lat": 40.65078
+    "lat": 40.65078,
+    "longName": "Hendrix Creek by 26th Ward Outfall"
   },
   {
-    "shortname": "HC3",
+    "shortName": "HC3",
     "lng": -73.86756,
-    "lat": 40.63856
+    "lat": 40.63856,
+    "longName": "Hendrix Creek under Belt Pkwy"
   },
   {
-    "shortname": "HOB",
-    "lng": -74.19583,
-    "lat": 40.6375
-  },
-  {
-    "shortname": "HR03",
-    "lng": -73.82087,
-    "lat": 40.88087
-  },
-  {
-    "shortname": "HR1",
-    "lng": -73.82131,
-    "lat": 40.8687
-  },
-  {
-    "shortname": "HR2",
-    "lng": -73.82403,
-    "lat": 40.88791
-  },
-  {
-    "shortname": "J1",
-    "lng": -73.88467,
-    "lat": 40.57333
-  },
-  {
-    "shortname": "J10",
+    "shortName": "J10",
     "lng": -73.89472,
-    "lat": 40.62167
+    "lat": 40.62167,
+    "longName": "Paerdegat Basin"
   },
   {
-    "shortname": "J11",
+    "shortName": "J11",
     "lng": -73.93133,
-    "lat": 40.58167
+    "lat": 40.58167,
+    "longName": "Sheepshead Bay"
   },
   {
-    "shortname": "J12",
+    "shortName": "J12",
     "lng": -73.80633,
-    "lat": 40.633
+    "lat": 40.633,
+    "longName": "Grassy Bay"
   },
   {
-    "shortname": "J14",
+    "shortName": "J14",
     "lng": -73.82378,
-    "lat": 40.60917
+    "lat": 40.60917,
+    "longName": "West of Broad Channel"
   },
   {
-    "shortname": "J16",
+    "shortName": "J16",
     "lng": -73.84761,
-    "lat": 40.61143
+    "lat": 40.61143,
+    "longName": "Horse Channel"
   },
   {
-    "shortname": "J18",
-    "lng": -73.84795,
-    "lat": 40.61932
-  },
-  {
-    "shortname": "J2",
+    "shortName": "J2",
     "lng": -73.88583,
-    "lat": 40.60805
+    "lat": 40.60805,
+    "longName": "Mill Basin"
   },
   {
-    "shortname": "J3",
+    "shortName": "J3",
     "lng": -73.88283,
-    "lat": 40.627
+    "lat": 40.627,
+    "longName": "Canarsie Pier"
   },
   {
-    "shortname": "J5",
+    "shortName": "J5",
     "lng": -73.809,
-    "lat": 40.59767
+    "lat": 40.59767,
+    "longName": "Railroad Trestle"
   },
   {
-    "shortname": "J7",
+    "shortName": "J7",
     "lng": -73.82083,
-    "lat": 40.64533
+    "lat": 40.64533,
+    "longName": "Mouth of Bergen Basin"
   },
   {
-    "shortname": "J8",
+    "shortName": "J8",
     "lng": -73.85117,
-    "lat": 40.64283
+    "lat": 40.64283,
+    "longName": "Spring Creek"
   },
   {
-    "shortname": "J9A",
+    "shortName": "J9A",
     "lng": -73.87367,
-    "lat": 40.63383
+    "lat": 40.63383,
+    "longName": "Fresh Creek Buoy \"C21"
   },
   {
-    "shortname": "JA1",
+    "shortName": "JA1",
     "lng": -73.813,
-    "lat": 40.643
+    "lat": 40.643,
+    "longName": "Jamaica WWTP Outfall"
   },
   {
-    "shortname": "K1",
-    "lng": -74.08195,
-    "lat": 40.65111
-  },
-  {
-    "shortname": "K2",
-    "lng": -74.153,
-    "lat": 40.64117
-  },
-  {
-    "shortname": "K3",
-    "lng": -74.19583,
-    "lat": 40.6375
-  },
-  {
-    "shortname": "K4",
-    "lng": -74.21167,
-    "lat": 40.567
-  },
-  {
-    "shortname": "K5",
-    "lng": -74.25883,
-    "lat": 40.50883
-  },
-  {
-    "shortname": "K5A",
-    "lng": -74.24583,
-    "lat": 40.4845
-  },
-  {
-    "shortname": "K6",
-    "lng": -74.10083,
-    "lat": 40.51033
-  },
-  {
-    "shortname": "LN1",
-    "lng": -73.76083,
-    "lat": 40.778
-  },
-  {
-    "shortname": "N1",
-    "lng": -73.91617,
-    "lat": 40.9145
-  },
-  {
-    "shortname": "N16",
-    "lng": -73.94367,
-    "lat": 40.52967
-  },
-  {
-    "shortname": "N3B",
-    "lng": -73.96533,
-    "lat": 40.82083
-  },
-  {
-    "shortname": "N3C",
-    "lng": -73.9815,
-    "lat": 40.79317
-  },
-  {
-    "shortname": "N4",
-    "lng": -73.9815,
-    "lat": 40.79317
-  },
-  {
-    "shortname": "N5",
-    "lng": -74.03,
-    "lat": 40.7045
-  },
-  {
-    "shortname": "N6",
-    "lng": -74.05133,
-    "lat": 40.66467
-  },
-  {
-    "shortname": "N7",
-    "lng": -74.05383,
-    "lat": 40.64383
-  },
-  {
-    "shortname": "N8",
-    "lng": -74.0455,
-    "lat": 40.60617
-  },
-  {
-    "shortname": "N9",
-    "lng": -73.98333,
-    "lat": 40.56833
-  },
-  {
-    "shortname": "N9A",
+    "shortName": "N9A",
     "lng": -73.9355,
-    "lat": 40.56783
+    "lat": 40.56783,
+    "longName": "Coney Is. Outfall"
   },
   {
-    "shortname": "NC",
-    "lng": -73.92317,
-    "lat": 40.71717
-  },
-  {
-    "shortname": "NC0",
-    "lng": -73.93156,
-    "lat": 40.71496
-  },
-  {
-    "shortname": "NC1",
-    "lng": -73.92704,
-    "lat": 40.72455
-  },
-  {
-    "shortname": "NC2",
-    "lng": -73.9355,
-    "lat": 40.72883
-  },
-  {
-    "shortname": "NC3",
-    "lng": -73.94617,
-    "lat": 40.73717
-  },
-  {
-    "shortname": "NR1",
-    "lng": -73.95816,
-    "lat": 40.82633
-  },
-  {
-    "shortname": "PB2",
+    "shortName": "PB2",
     "lng": -73.90967,
-    "lat": 40.628
+    "lat": 40.628,
+    "longName": "Middle of Paerdegat Basin"
   },
   {
-    "shortname": "PB3",
+    "shortName": "PB3",
     "lng": -73.89983,
-    "lat": 40.62342
+    "lat": 40.62342,
+    "longName": "Mouth of Paerdegat Basin"
   },
   {
-    "shortname": "SP1",
+    "shortName": "SP1",
     "lng": -73.86029,
-    "lat": 40.65507
+    "lat": 40.65507,
+    "longName": "Spring Creek Head"
   },
   {
-    "shortname": "SP2",
+    "shortName": "SP2",
     "lng": -73.85417,
-    "lat": 40.6445
-  },
-  {
-    "shortname": "TB1",
-    "lng": -73.74899,
-    "lat": 40.64397
-  },
-  {
-    "shortname": "TB2",
-    "lng": -73.74764,
-    "lat": 40.63576
-  },
-  {
-    "shortname": "TR1",
-    "lng": -73.9875,
-    "lat": 40.78583
-  },
-  {
-    "shortname": "TR2",
-    "lng": -73.97867,
-    "lat": 40.79717
-  },
-  {
-    "shortname": "WC1",
-    "lng": -73.84228,
-    "lat": 40.82775
-  },
-  {
-    "shortname": "WC2",
-    "lng": -73.83984,
-    "lat": 40.83888
-  },
-  {
-    "shortname": "WC3",
-    "lng": -73.84319,
-    "lat": 40.81507
+    "lat": 40.6445,
+    "longName": "Spring Creek Head"
   }
 ]
--- scripts.js
+++ scripts.js
@@ -17,8 +17,38 @@ map.addControl(new mapboxgl.NavigationControl());
 
 //assign map markers from dataset
 testlocationData.forEach(function(locationEntry) {
-  console.log(locationEntry.shortname, locationEntry.lat)
+  console.log(locationEntry.shortName, locationEntry.lat)
   new mapboxgl.Marker()
     .setLngLat([locationEntry.lng, locationEntry.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+     .setHTML(`${locationEntry.longName}, ${locationEntry.lng}, ${locationEntry.lat}`))
     .addTo(map);
+})
+//add map layer of circles for test locations
+map.on('style.load', function() {
+  //add geojson data for test locations
+  map.addSource('circleData', {
+    type: 'geojson',
+    data: './map.geojson',
+  })
+  // styling for circles
+  map.addLayer({
+    id: 'data',
+    type: 'circle',
+    source: 'circleData',
+    paint: {
+      'circle-color': 'brown',
+      'circle-radius': {
+        property: 'fecalLevel',
+        stops: [
+          [0, 10],
+          [300, 20],
+          [700, 30],
+          [1000, 40],
+        ]
+      },
+      'circle-stroke-width': 1,
+      'circle-stroke-color': 'black'
+    }
+  })
 })
