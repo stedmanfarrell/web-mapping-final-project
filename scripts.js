cec4378d92fe5f9fc90d74e9a5577fe1c1c0b6d0
wsfpacileo <wsfpacileo@gmail.com>
2020-03-21 00:42:29 -0400

    added demo slider
    Co-Authored-By: null <stedmanfarrell@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // this is Wayne's mapboxGL token
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2F5bmVwYWNpbGVvIiwiYSI6ImNrNm9jc281YzA4enEza21pdDcybjFybWgifQ.AETmAWEwFz3mIrpMCDGsGQ';
 
-var initialCenterPoint = [-73.913042,40.599552]
+var initialCenterPoint = [-73.913042, 40.599552]
 var initialZoom = 11
 
 var initOptions = {
@@ -16,39 +16,46 @@ var map = new mapboxgl.Map(initOptions);
 map.addControl(new mapboxgl.NavigationControl());
 
 //assign map markers from dataset
-testlocationData.forEach(function(locationEntry) {
-  console.log(locationEntry.shortName, locationEntry.lat)
-  new mapboxgl.Marker()
-    .setLngLat([locationEntry.lng, locationEntry.lat])
-    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-     .setHTML(`${locationEntry.longName}, ${locationEntry.lng}, ${locationEntry.lat}`))
-    .addTo(map);
-})
+//testlocationData.forEach(function(locationEntry) {
+//  console.log(locationEntry.shortName, locationEntry.lat)
+//  new mapboxgl.Marker()
+//    .setLngLat([locationEntry.lng, locationEntry.lat])
+//    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+//    .setHTML(`${locationEntry.longName}, ${locationEntry.lng}, ${locationEntry.lat}`))
+//    .addTo(map);
+//})
 //add map layer of circles for test locations
 map.on('style.load', function() {
-  //add geojson data for test locations
-  map.addSource('circleData', {
-    type: 'geojson',
-    data: './map.geojson',
-  })
-  // styling for circles
-  map.addLayer({
-    id: 'data',
-    type: 'circle',
-    source: 'circleData',
-    paint: {
-      'circle-color': 'brown',
-      'circle-radius': {
-        property: 'fecalLevel',
-        stops: [
-          [0, 10],
-          [300, 20],
-          [700, 30],
-          [1000, 40],
-        ]
-      },
-      'circle-stroke-width': 1,
-      'circle-stroke-color': 'black'
-    }
-  })
-})
+
+        //add geojson data for test locations
+        map.addSource('circleData', {
+          type: 'geojson',
+          data: './map.geojson',
+        })
+        // styling for circles
+        map.addLayer({
+          id: 'data',
+          type: 'circle',
+          source: 'circleData',
+          paint: {
+            'circle-color': 'brown',
+            'circle-radius': {
+              property: 'fecalLevel',
+              stops: [
+                [0, 10],
+                [300, 20],
+                [700, 30],
+                [1000, 40],
+              ]
+            },
+            'circle-stroke-width': 1,
+            'circle-stroke-color': 'black',
+          }
+        })
+        map.setFilter('data', ['==', 'hour', 20]);
+        document.getElementById('slider').addEventListener('input', function(e) {
+          var hour = parseInt(e.target.value);
+          //update the map
+          map.setFilter('data', ['==', ['number', ['get', 'hour']], hour]);
+        })
+      })
